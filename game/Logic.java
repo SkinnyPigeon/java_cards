7dcf4406d9b80b7ee1570060e9253be113b6e96b
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-08-12 12:42:29 +0100

    about to try to build a new arraylist of the two piles of cards

--- game/Logic.java
+++ game/Logic.java
@@ -1,4 +1,16 @@
 package Game;
 import java.util.*;
 
+public class Logic {
 
+  private ArrayList< String > playerCards;
+  private ArrayList< String > tableCards;
+
+  public Logic( ArrayList playerCards, ArrayList tableCards ) {
+    this.playerCards = playerCards;
+    this.tableCards = tableCards;
+  }
+
+
+
+}
